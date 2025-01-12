71b1ef7fb095c9eeacb4f4d7b20267c0db76fc5a
GNithish10 <128487193+GNithish10@users.noreply.github.com>
2025-01-12 13:51:55 +0530

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -5,22 +5,18 @@ def merge(arr, left, mid, right):
     n1 = mid - left + 1
     n2 = right - mid
 
-    # Create temp arrays
     L = [0] * n1
     R = [0] * n2
 
-    # Copy data to temp arrays L[] and R[]
     for i in range(n1):
         L[i] = arr[left + i]
     for j in range(n2):
         R[j] = arr[mid + 1 + j]
 
-    i = 0  # Initial index of first subarray
-    j = 0  # Initial index of second subarray
-    k = left  # Initial index of merged subarray
+    i = 0  
+    j = 0  
+    k = left  
 
-    # Merge the temp arrays back
-    # into arr[left..right]
     while i < n1 and j < n2:
         if L[i] <= R[j]:
             arr[k] = L[i]
@@ -30,15 +26,11 @@ def merge(arr, left, mid, right):
             j += 1
         k += 1
 
-    # Copy the remaining elements of L[],
-    # if there are any
     while i < n1:
         arr[k] = L[i]
         i += 1
         k += 1
 
-    # Copy the remaining elements of R[], 
-    # if there are any
     while j < n2:
         arr[k] = R[j]
         j += 1
